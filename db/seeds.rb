c03a588d4462baad05bc56dfe451589e2e7ec316
PROUDFEETS <sgabisouza@gmail.com>
2019-05-07 15:57:58 -0400

    add seeds, without typos this time

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-animal1 = Animal.create(petfinder_id: 1, type: "dog", location: "Brookline, MA", zipcode: "02446", photo: "picture1", tstring:"Fluffy is a charming dog, oh wow")
-animal2 = Animal.create(petfinder_id: 2, type: "cat", location: "Providence, RI", zipcode: "02902", photo: "picture2", tstring:"Ms Meowzers is such a good cat, nevermind the claws")
-animal3 = Animal.create(petfinder_id: 3, type: "bird", location: "Austin, TX", zipcode: "73301", photo: "picture3", tstring:"This bird is a demon and screams 24/7")
+animal1 = Animal.create(petfinder_id: 1, species: "dog", location: "Brookline, MA", zipcode: "02446", photo: "picture1", description:"Fluffy is a charming dog, oh wow")
+animal2 = Animal.create(petfinder_id: 2, species: "cat", location: "Providence, RI", zipcode: "02902", photo: "picture2", description:"Ms Meowzers is such a good cat, nevermind the claws")
+animal3 = Animal.create(petfinder_id: 3, species: "bird", location: "Austin, TX", zipcode: "73301", photo: "picture3", description:"This bird is a demon and screams 24/7")
 
 user1 = User.create(email: "eevee@gmail.com")
 user2 = User.create(email: "pikachu@gmail.com")
